516030b7a2ac2a2e71058b7a42268652b0759600
Caleb R <cjr4912@gmail.com>
2020-11-09 16:48:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(some_student)
+    roster << some_student
+  end
+  
+end
