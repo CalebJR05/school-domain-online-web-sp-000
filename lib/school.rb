5cd0828aeee284d51c63ec1bb60581fa679a059f
Caleb R <cjr4912@gmail.com>
2020-11-09 16:52:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,6 @@ class School
   end 
   
   def add_student(student, level)
-    # this sort of means #=> a || a = b ... or #=> x || x = y
-    # it's freak'n weird
     roster[level] ||= []
     roster[level] << student
   end 
@@ -21,4 +19,13 @@ class School
     end 
   end 
   
+  def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
   
+end 
+  
+end 
